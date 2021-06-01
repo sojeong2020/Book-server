04c5e75e9638cf484565169e4e76a516247cd263
Sojeong Park <macpark09@gmail.com>
2021-06-01 17:07:33 +0100

    add

--- server.js
+++ server.js
@@ -5,27 +5,26 @@ const server = http.createServer((request, response) => {
   const{method, url} =request;
   if(url === '/api' && method === 'GET'){
   response.setHeader("Content-Type", "application/json");
-  response.statusCode = 418;
   response.statusCode = 200;
   response.write(JSON.stringify({ msg: "Hello Mr.Get Request" }));
   response.end();
   }
-  else if (url === '/api/books' && method === 'GET'){
-    const booksContents = fs.readFile('data/books.json','utf8'(err,contents)=>{
-
-
-    booksContents.then((contents)=>{
-      const parsedContents= JSON.parse(contents);
-      console.log(parsedContents);
-      response.setHeader("Content-Type", "application/json");
-      response.statusCode = 200;
-      response.write(JSON.parse(contents));
-      response.end();
-
+   else if(url === '/api/books' && method === 'GET'){
+    fs.readFile('data/books.json','utf-8',(err,fileContents)=>{
+      if(err) console.log(err);
+      else{
+        const parsedContents = JSON.parse(fileContents);
+        console.log(parsedContents);
+        response.setHeader("Content-Type", "application/json");
+        response.statusCode = 200;
+        response.write(JSON.stringify({books: parsedContents}));
+        response.end();
+      }
     })
-
-  })
+   }
 
 });
 server.listen(5656, (err) => {
   if (err) console.log(err);
+  else console.log('Server listening on port: 5656')
+});
